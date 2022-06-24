10aaabc6c07055cf6529dd4f19e7a10ef6913588
Francesco Miranda <francescomiranda@Francescos-MacBook-Pro.local>
2022-06-24 17:42:55 +0100

    added initialization for modal a custom css

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // sidenav initialization
-    let sidenav = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(sidenav);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  // sidenav initialization
+  let sidenav = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(sidenav);
+
+  // Initialization modal
+  let modal = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(modal);
+});
+
+
