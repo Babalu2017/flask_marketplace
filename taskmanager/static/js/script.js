d82571244383b4f4a0c47e0e393bd7fc9ac97229
Francesco Miranda <francescomiranda@Francescos-MacBook-Pro.local>
2022-06-24 18:26:58 +0100

    added add_task page and functionalities

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -6,6 +6,18 @@ document.addEventListener('DOMContentLoaded', function () {
   // Initialization modal
   let modal = document.querySelectorAll('.modal');
   var instances = M.Modal.init(modal);
+
+  // datepicker initialization
+  let datepicker = document.querySelectorAll(".datepicker");
+  M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+  });
+
+  // select initialization
+  let selects = document.querySelectorAll("select");
+  M.FormSelect.init(selects);
+  
 });
 
 
