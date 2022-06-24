bfb303da989517c29bd8761ea96a057bbf9b3c77
Francesco Miranda <francescomiranda@Francescos-MacBook-Pro.local>
2022-06-24 21:58:28 +0100

    initialized collapsible

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -10,14 +10,18 @@ document.addEventListener('DOMContentLoaded', function () {
   // datepicker initialization
   let datepicker = document.querySelectorAll(".datepicker");
   M.Datepicker.init(datepicker, {
-      format: "dd mmmm, yyyy",
-      i18n: {done: "Select"}
+    format: "dd mmmm, yyyy",
+    i18n: { done: "Select" }
   });
 
   // select initialization
   let selects = document.querySelectorAll("select");
   M.FormSelect.init(selects);
-  
+
+  // Initialization Collapsible
+  let collap = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(collap);
+
 });
 
 
